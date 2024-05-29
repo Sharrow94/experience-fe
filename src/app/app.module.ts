ef9a8a306aeb85c7b1149bdee168ffe932380148
Sharrow94 <maciej.roszyk@gmail.com>
2024-05-29 11:37:43 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarTopComponent } from './navbar/navbar-top/navbar-top.component';
+import { NavbarSideComponent } from './navbar/navbar-side/navbar-side.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { CvComponent } from './cv/cv.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarTopComponent,
+    NavbarSideComponent,
+    CvComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/navbar/navbar-top/navbar-top.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-top',
+  templateUrl: './navbar-top.component.html',
+  styleUrl: './navbar-top.component.css'
+})
+export class NavbarTopComponent {
+
+}
